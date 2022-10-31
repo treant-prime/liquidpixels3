fb74364c03f0e154c45dd456bd391ebaf1c7e02a
Łukasz Kałużny <lukasz@enginearch.com>
2022-10-31 17:38:04 +0100

    Deploy

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,10 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/liquidpixels3/",
   plugins: [vue()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
-  },
-  build: {
-    outDir: "./docs",
   },
 });
