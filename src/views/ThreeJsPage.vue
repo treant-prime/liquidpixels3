1f5d486088b1bc0ec659e9becd089cb994ce33db
Łukasz Kałużny <lukasz@enginearch.com>
2022-10-31 15:05:48 +0100

    Three JS attempts

--- src/views/ThreeJsPage.vue
+++ src/views/ThreeJsPage.vue
@@ -1,54 +1,60 @@
 <script lang="ts">
-// import { ref } from "vue";
 // import * as Three from "three";
 
 export default {
   name: "ThreeJsPage",
   props: {},
+  created() {
+    // this.scene.add(this.camera);
+    // this.scene.add(this.light);
+    // this.scene.add(this.cube);
+    // this.renderer.setSize(window.innerWidth, window.innerHeight);
+    // this.light.position.set(0, 0, 10);
+    // this.camera.position.z = 5;
+    // this.scene.background = new Three.Color("hsl(0, 100%, 100%)");
+  },
   mounted() {
-    // this.init();
+    // this.$refs.canvas.appendChild(this.renderer.domElement);
     // this.animate();
   },
   methods: {
-    init: function () {
-      // const container = document.getElementById("container");
-      // this.camera = new Three.PerspectiveCamera(
-      //   70,
-      //   container.clientWidth / container.clientHeight,
-      //   0.01,
-      //   10
-      // );
-      // this.camera.position.z = 1;
-      // this.scene = new Three.Scene();
-      // const geometry = new Three.BoxGeometry(0.2, 0.2, 0.2);
-      // const material = new Three.MeshNormalMaterial();
-      // this.mesh = new Three.Mesh(geometry, material);
-      // this.scene.add(this.mesh);
-      // this.renderer = new Three.WebGLRenderer({ antialias: true });
-      // this.renderer.setSize(container.clientWidth, container.clientHeight);
-      // container.appendChild(this.renderer.domElement);
-    },
-    animate: function () {
-      // requestAnimationFrame(this.animate);
-      // this.mesh.rotation.x += 0.01;
-      // this.mesh.rotation.y += 0.02;
-      // this.renderer.render(this.scene, this.camera);
-    },
+    // animate() {
+    //   requestAnimationFrame(this.animate);
+    //   this.renderer.render(this.scene, this.camera);
+    //   this.cube.rotation.y += this.speed;
+    // },
   },
-  // setup() {},
   data() {
-    return {
-      camera: null,
-      scene: null,
-      renderer: null,
-      mesh: null,
-    };
+    // const scene = new Three.Scene();
+    // const camera = new Three.PerspectiveCamera(
+    //   75,
+    //   window.innerWidth / window.innerHeight,
+    //   0.1,
+    //   1000
+    // );
+    // const renderer = new Three.WebGLRenderer({ antialias: true });
+    // const light = new Three.DirectionalLight("hsl(0, 100%, 100%)");
+    // const geometry = new Three.BoxGeometry(0.2, 0.2, 0.2);
+    // const material = new Three.MeshStandardMaterial({
+    //   side: Three.FrontSide,
+    //   color: "hsl(0, 100%, 50%)",
+    //   wireframe: false,
+    // });
+    // const cube = new Three.Mesh(geometry, material);
+    // return {
+    //   scene: scene,
+    //   camera: camera,
+    //   controls: [],
+    //   renderer: renderer,
+    //   light: light,
+    //   cube: cube,
+    // };
   },
 };
 </script>
 
 <template>
-  <div id="container"></div>
+  <div ref="canvas"></div>
 </template>
 
 <style scoped></style>
